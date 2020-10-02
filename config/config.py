51ded50c1ed7d14bff28e5ae1522c2bf1379c952
Марк Соболев <44925911+Cesoneemz@users.noreply.github.com>
2020-10-02 14:57:11 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,7 +6,7 @@ API_TOKEN = getenv('API_KEY')
 POSTGRES_CONFIG = {
     'host': getenv('POSTGRES_HOST'),
     'user': getenv('POSTGRES_USER'),
-    'password': getenv('POSTGRES_PASSWORD),
+    'password': getenv('POSTGRES_PASSWORD'),
     'database': getenv('POSTGRES_DATABASE_FOUR'),
     'port': getenv('POSTGRES_PORT')
         }
